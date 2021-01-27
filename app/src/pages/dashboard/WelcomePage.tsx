2d17994ccb8cda5bd2a98c07f54e7cd1720f4203
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2021-01-27 17:00:54 +0100

    Added Welcome Page

--- app/src/pages/dashboard/WelcomePage.tsx
+++ app/src/pages/dashboard/WelcomePage.tsx
@@ -1,9 +1,32 @@
+import { Box, Button, Heading, Icon, Text, VStack } from "@chakra-ui/react";
 import React from "react";
+import { HiPlus } from "react-icons/hi";
 
 interface WelcomeProps {}
 
 const WelcomePage = (props: WelcomeProps) => {
-  return <div></div>;
+  return (
+    <Box minHeight="100vh">
+      <Box width={{ lg: "40%" }}>
+        <VStack spacing={4} alignItems="flex-start">
+          <Heading>Welcome</Heading>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui saepe
+            sequi voluptatem repellat adipisci quam libero numquam, nisi laborum
+            consectetur a mollitia. Similique, corrupti est deserunt consectetur
+            eos consequatur perspiciatis!
+          </Text>
+          <Button
+            rightIcon={<Icon as={HiPlus} />}
+            bgColor="primary.base"
+            colorScheme="green"
+          >
+            Create a new note
+          </Button>
+        </VStack>
+      </Box>
+    </Box>
+  );
 };
 
 export default WelcomePage;
