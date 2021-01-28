03ee4b9a53747bb4ab0040a7fb9f72d0eb70520d
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2021-01-28 08:19:07 +0100

    Integrated Apollo Client

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
