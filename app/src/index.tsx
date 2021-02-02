37bf0ca3160cf819437480be5b82558cf34c0975
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2021-02-02 17:12:32 +0100

    :beetle: Fixed authentication header bug

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -10,26 +10,94 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
-import { accesToken } from "./store/context/auth";
+import { setContext } from "@apollo/client/link/context";
+import { getAccessToken, setAccessToken } from "./store/global/accessToken";
+import { TokenRefreshLink } from "apollo-link-token-refresh";
+import jwtDecode from "jwt-decode";
 
-const link = onError(({ graphQLErrors, networkError, response }) => {
+const apolloURI =
+  process.env.REACT_APP_APOLLO_SERVER_URI ?? "http://localhost:5000";
+
+const refreshLink = new TokenRefreshLink({
+  accessTokenField: "accessToken",
+  isTokenValidOrUndefined: () => {
+    const token = getAccessToken();
+
+    if (!token) return true;
+
+    try {
+      const { exp }: { exp: number } = jwtDecode(token);
+      if (Date.now() >= exp * 1000) {
+        return false;
+      } else {
+        return true;
+      }
+    } catch (err) {
+      return false;
+    }
+  },
+  fetchAccessToken: async () => {
+    const resp = await fetch(apolloURI, {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${getAccessToken()}` || "",
+        "content-type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        query: `mutation RefreshMyToken {
+        refreshMyToken {
+          status
+          message
+          accessToken
+        }
+      }`,
+      }),
+    });
+    return resp.json();
+  },
+  handleResponse: (operation, accessTokenField) => (response: any) => {
+    return response.data.refreshMyToken;
+  },
+  handleFetch: (accessToken: string) => {
+    setAccessToken(accessToken);
+  },
+  handleError: (err: Error) => {
+    console.warn("Your refresh token is invalid. Try to relogin");
+    console.error(err);
+  },
+});
+
+const errLink = onError(({ graphQLErrors, networkError, response }) => {
   graphQLErrors && console.log("⚛️ [GraphQl Error]:", graphQLErrors);
   networkError && console.log("👮🏻‍♀️ [Network error]:", networkError);
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = getAccessToken();
+  if (!token) return headers;
+
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${token}`,
+    },
+  };
+});
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
+  name: "react-web-client",
   link: ApolloLink.from([
-    link,
-    new HttpLink({
-      uri: process.env.REACT_APP_APOLLO_SERVER_URI,
-      credentials: "include",
-    }),
+    refreshLink,
+    errLink,
+    authLink.concat(
+      new HttpLink({
+        uri: apolloURI,
+        credentials: "include",
+      })
+    ),
   ]),
-  credentials: "include",
-  headers: {
-    authorization: `Bearer ${accesToken}` || "",
-  },
 });
 
 ReactDOM.render(
