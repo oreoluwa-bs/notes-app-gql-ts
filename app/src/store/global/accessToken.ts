6a182d5c3e72ea2ed0dfe7bf33ad4756432af4e9
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2021-02-03 03:20:55 +0100

    :wrench: Made a change

--- app/src/store/global/accessToken.ts
+++ app/src/store/global/accessToken.ts
@@ -4,7 +4,6 @@ export const setAccessToken = (s: string) => {
   accessToken = s;
 };
 
-export const getAccessToken = (s?: string) => {
-  s && console.log(s);
+export const getAccessToken = () => {
   return accessToken;
 };
