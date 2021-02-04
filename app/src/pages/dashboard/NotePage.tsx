1d87ec4585574a6cdab7f8f425170b09aa126d0c
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2021-02-04 21:03:32 +0100

    :zap: Implemented Delete and Update note features

--- app/src/pages/dashboard/NotePage.tsx
+++ app/src/pages/dashboard/NotePage.tsx
@@ -1,11 +1,51 @@
-import { Box } from "@chakra-ui/react";
+import { gql, useQuery } from "@apollo/client";
+import { Box, Button } from "@chakra-ui/react";
+import { useContext, useState } from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { NoteContext, NoteContextType } from "../../store/context/note";
 
-interface NotePageProps {}
+interface NotePageProps {
+  noteslug?: string;
+}
 
-const NotePage = (props: NotePageProps) => {
+const GET_NOTE = gql`
+  query GetNote($slug: String) {
+    note(filter: { slug: $slug }) {
+      id
+      title
+      slug
+      content
+    }
+  }
+`;
+
+const NotePage = (props: RouteComponentProps<NotePageProps>) => {
+  const { history, match } = props;
+  const { handleUpdateNote, handleDeleteNote } = useContext(
+    NoteContext
+  ) as NoteContextType;
+  const { loading, data, refetch } = useQuery(GET_NOTE, {
+    variables: { slug: match.params?.noteslug },
+  });
+
+  const [noteData] = useState({ content: "Content" });
+
+  const handleUpdate = async () => {
+    await handleUpdateNote({ noteID: data.note.id, noteData });
+    await refetch();
+  };
+
+  const handleDelete = async () => {
+    await handleDeleteNote(data.note.id);
+    history.push("/app");
+  };
+
+  if (loading) return null;
   return (
-    <Box bg="blue.100" minHeight="100vh" py="30px">
+    <Box minHeight="100vh" py="30px">
       {/* Text Editor */}
+      <Button onClick={handleUpdate}>UpdateNote</Button>
+      <Button onClick={handleDelete}>DeleteNote</Button>
     </Box>
   );
 };
