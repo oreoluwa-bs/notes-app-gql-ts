6d85febb64b3b6be50ec1705e3ae02919dd07c3d
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2021-02-10 12:24:29 +0100

    :wrench: Made performance opitimization

--- app/src/pages/dashboard/NotePage.tsx
+++ app/src/pages/dashboard/NotePage.tsx
@@ -12,14 +12,17 @@ import {
   Tooltip,
 } from "@chakra-ui/react";
 import { convertFromRaw, EditorState } from "draft-js";
-import { useContext, useEffect, useState } from "react";
+import { lazy, Suspense, useContext, useEffect, useState } from "react";
 import { useHistory, useRouteMatch } from "react-router-dom";
-import RichTextEditor from "../../components/RichTextEditor";
+// import RichTextEditor from "../../components/RichTextEditor";
 import { NoteContext, NoteContextType } from "../../store/context/note";
 import { FiMoreHorizontal } from "react-icons/fi";
-import { HiDocumentDownload, HiTrash } from "react-icons/hi";
+import { HiDocumentDownload, HiSpeakerphone, HiTrash } from "react-icons/hi";
 import { transformNoteData } from "../../helpers/note";
-import TextToSpeech from "../../components/TextToSpeech";
+// import TextToSpeech from "../../components/TextToSpeech";
+
+const RichTextEditor = lazy(() => import("../../components/RichTextEditor"));
+const TextToSpeech = lazy(() => import("../../components/TextToSpeech"));
 
 interface NotePageProps {
   noteslug?: string;
@@ -95,84 +98,88 @@ const NotePage = ({ isSideNavOpen }: NotePageProps) => {
   return (
     <Box minHeight="100vh" pb="30px" width="100%">
       {/* Text Editor */}
-      <RichTextEditor
-        editorState={editorState}
-        setEditorState={setEditorState}
-        isSideNavOpen={isSideNavOpen}
-        actions={[
-          isSpeaking ? (
-            <Box key="speak" display="inline-block">
-              <TextToSpeech
-                handleOnFinishSpeaking={() => setIsSpeaking(false)}
-                utterText={editorState.getCurrentContent().getPlainText()}
-              />
-            </Box>
-          ) : undefined,
-          <Box key="more options" display="inline-block">
-            <Menu key="more options">
-              <Tooltip label="More Options">
-                <MenuButton
-                  as={IconButton}
-                  aria-label="More"
-                  icon={<Icon as={FiMoreHorizontal} />}
-                />
-              </Tooltip>
-              <MenuList>
-                <MenuItem
-                  icon={<Icon as={HiDocumentDownload} />}
-                  onClick={() => {
-                    const element = document.createElement("a");
+      <Suspense fallback={<div>Loading...</div>}>
+        <RichTextEditor
+          editorState={editorState}
+          setEditorState={setEditorState}
+          isSideNavOpen={isSideNavOpen}
+          actions={[
+            isSpeaking ? (
+              <Box key="speak" display="inline-block">
+                <Suspense fallback={<div>Loading...</div>}>
+                  <TextToSpeech
+                    handleOnFinishSpeaking={() => setIsSpeaking(false)}
+                    utterText={editorState.getCurrentContent().getPlainText()}
+                  />
+                </Suspense>
+              </Box>
+            ) : undefined,
+            <Box key="more options" display="inline-block">
+              <Menu key="more options">
+                <Tooltip label="More Options">
+                  <MenuButton
+                    as={IconButton}
+                    aria-label="More"
+                    icon={<Icon as={FiMoreHorizontal} />}
+                  />
+                </Tooltip>
+                <MenuList>
+                  <MenuItem
+                    icon={<Icon as={HiDocumentDownload} />}
+                    onClick={() => {
+                      const element = document.createElement("a");
 
-                    const dataa = editorState
-                      .getCurrentContent()
-                      .getPlainText("")
-                      .split(" ")
-                      .join(" ");
+                      const dataa = editorState
+                        .getCurrentContent()
+                        .getPlainText("")
+                        .split(" ")
+                        .join(" ");
 
-                    const file = new Blob([dataa], {
-                      type: "text/plain",
-                    });
+                      const file = new Blob([dataa], {
+                        type: "text/plain",
+                      });
 
-                    element.href = URL.createObjectURL(file);
-                    element.download = data.note.title;
-                    document.body.appendChild(element);
-                    element.click();
-                    console.log(dataa);
-                  }}
-                >
-                  Download Note as txt
-                </MenuItem>
-                <MenuItem
-                  icon={<Icon as={HiTrash} />}
-                  onClick={() => {
-                    if ("speechSynthesis" in window) {
-                      setIsSpeaking(true);
-                    } else {
-                      alert(
-                        "Sorry, your browser doesn't support this feature!"
-                      );
-                    }
-                  }}
-                >
-                  Read Note Aloud
-                </MenuItem>
-                <MenuItem icon={<Icon as={HiTrash} />} onClick={handleDelete}>
-                  Delete Note
-                </MenuItem>
-                <MenuItem icon={<Icon as={HiTrash} />} onClick={handleUpdate}>
-                  Update Note
-                </MenuItem>
-                {/* <MenuItem command="⌘N">New Window</MenuItem>
+                      element.href = URL.createObjectURL(file);
+                      element.download = data.note.title;
+                      document.body.appendChild(element);
+                      element.click();
+                      console.log(dataa);
+                    }}
+                  >
+                    Download Note as txt
+                  </MenuItem>
+                  <MenuItem
+                    icon={<Icon as={HiSpeakerphone} />}
+                    onClick={() => {
+                      if ("speechSynthesis" in window) {
+                        setIsSpeaking(true);
+                      } else {
+                        alert(
+                          "Sorry, your browser doesn't support this feature!"
+                        );
+                      }
+                    }}
+                  >
+                    Read Note Aloud
+                  </MenuItem>
+                  <MenuItem icon={<Icon as={HiTrash} />} onClick={handleDelete}>
+                    Delete Note
+                  </MenuItem>
+                  <MenuItem icon={<Icon as={HiTrash} />} onClick={handleUpdate}>
+                    Update Note
+                  </MenuItem>
+                  {/* <MenuItem command="⌘N">New Window</MenuItem>
               <MenuItem command="⌘⇧N">Open Closed Tab</MenuItem>
               <MenuItem command="⌘O">Open File...</MenuItem> */}
-              </MenuList>
-            </Menu>
-          </Box>,
-        ]}
-        onSave={() => {
-          console.log("save note");
-        }}
-      />
+                </MenuList>
+              </Menu>
+            </Box>,
+          ]}
+          onSave={() => {
+            console.log("save note");
+          }}
+        />
+      </Suspense>
     </Box>
   );
 };
